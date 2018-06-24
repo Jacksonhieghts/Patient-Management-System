9c16b2ea9e0b84ce5df3e72bd1a1d4b751c6d330
Admin <Admin@192.168.2.47>
2018-06-24 19:44:35 +0200

    nn

---
+++ index.php
@@ -0,0 +1,87 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html lang="zxx">
+
+<head>
+	<title>Portrait Login Form Responsive Widget Template :: w3layouts</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Portrait Login Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements"
+	/>
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- Meta tag Keywords -->
+	<!-- css files -->
+	<link rel="stylesheet" href="login_form/css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="login_form/css/font-awesome.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //css files -->
+	<!-- online-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Tangerine:400,700" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+	<!-- //online-fonts -->
+</head>
+
+<body>
+	<!--header-->
+	<div class="header-w3l">
+		<h1>
+			<span></span>Medi-Care Patient
+			<span></span>Management
+			</h1>
+	</div>
+	<!--//header-->
+	<div class="main-content-agile">
+		<div class="sub-main-w3">
+			<img src="login_form/images/profile.jpg" alt="" />
+			<h2>Patient Management System</h2>
+			<form action="login.php" method="post">
+				<div class="pom-agile">
+					<span class="fa fa-user-o" aria-hidden="true"></span>
+					<input placeholder="Username" name="username" class="user" type="text" required="">
+				</div>
+				<div class="pom-agile">
+					<span class="fa fa-key" aria-hidden="true"></span>
+					<input placeholder="Password" name="password" class="pass" type="password" required="">
+				</div>
+				<div class="sub-w3l">
+					<div class="sub-agile">
+						<input type="checkbox" id="brand1" value="">
+						<label for="brand1">
+							<span></span>Remember me</label>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="right-w3l">
+					<input type="submit" value="Login">
+				</div>
+				<div class="forgot-w3l">
+					<a href="email.php">Forgot Password?</a>
+				</div>
+			</form>
+		</div>
+	</div>
+	<!--//main-->
+	<!--footer-->
+	<div class="footer">
+		<p>&copy; 2018 Login Form. All rights reserved | Design by
+			<a href="#">Jack Msonkho</a>
+		</p>
+	</div>
+	<!--//footer-->
+</body>
+
+</html>
